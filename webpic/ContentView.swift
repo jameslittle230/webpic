c40df167cf01ad85b531b62d4a733311d0f7ca2c
James Little <littleguy23@gmail.com>
2020-03-01 13:56:12 -0800

    Initial Commit

---
+++ webpic/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  webpic
+//
+//  Created by James Little on 3/1/20.
+//  Copyright © 2020 James Little. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
